53cf1e8c1f4f53aa289d1bc431dc812387173d83
Skee <skee66499@Gmail.com>
2016-07-01 11:29:47 -0400

    Resubmittion: added the rand function because I did not realize we were allowed to include libraries for random generation

--- rps.c
+++ rps.c
@@ -1,13 +1,17 @@
 /*Sean Kee*/
 /*Rock Paper Scissors Game, Best of 5*/
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main()
 {
 	int playerScore = 0;
 	int cpuScore = 0;
 	char playerInput;
+	int cpuAction;
 	int round = 1;
+	int playerAction;
 
 	printf("Play Rock Paper Scissors, Best of 5.\nR: Rock\nP: Paper\nS: Scissors\n\n");
 
@@ -15,13 +19,22 @@ int main()
 	{
 		printf("#:");
 		scanf("%c", &playerInput);
-		
+		srand(time(0));
+		cpuAction = rand();
+		cpuAction = (cpuAction % 3) + 1;
+
 		switch(playerInput)
 		{
 			case 'r':
 			case 'R':
 				{
-					playerScore++;
+					playerAction = 1;
+					if(cpuAction > playerAction)
+						cpuScore++;
+					else
+						if(playerAction > cpuAction)
+							playerScore++;
+	
 					printf("Result:\nYour Score: %d\n CPU Score: %d\n", playerScore, cpuScore);
 					round++;
 					break;
@@ -29,7 +42,13 @@ int main()
 			case 'p':
 			case 'P':
 				{
-					cpuScore++;
+					playerAction = 2;
+					if(cpuAction > playerAction)
+						cpuScore++;
+					else
+						if(playerAction > cpuAction)
+							playerScore++;
+
 					printf("Result:\nYour Score: %d\n CPU Score: %d\n", playerScore, cpuScore);
 					round++;
 					break;
@@ -37,7 +56,13 @@ int main()
 			case 's':
 			case 'S':
 				{
-					cpuScore++;
+					playerAction = 3;
+					if(cpuAction > playerAction)
+						cpuScore++;
+					else
+						if(playerAction > cpuAction)
+							playerScore++;
+
 					printf("Result:\nYour Score: %d\n CPU Score: %d\n", playerScore, cpuScore);
 					round++;
 					break;
